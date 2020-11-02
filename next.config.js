9ed016b613b9c2a471b95c0e3a8a7023d395f2ee
Takenoko <takenoko.tech@gmail.com>
2020-11-02 23:21:44 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 console.log(`==================================`);
 console.log(`= Next config `);
 console.log(`==================================`);
-console.log(`process.env.GITHUB_PAGES = ${process.env.GITHUB_PAGES}`);
-console.log(`typeof = ${typeof process.env.GITHUB_PAGES}\n`)
+console.log(`process.env.GITHUB_PAGES = ${process.env.GITHUB_PAGES}(${typeof process.env.GITHUB_PAGES})`);
+console.log(`\n`)
 
 module.exports = {
     assetPrefix: process.env.GITHUB_PAGES == "true" ? "/apricot" : "",
