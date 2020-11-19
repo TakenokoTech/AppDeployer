b7ab0e2ded2b5af527a8cc6da7f53b219b8926e2
takenoko <takenoko.tech@gmail.com>
2020-11-20 02:21:13 +0900

    fix

--- src/components/Const.ts
+++ src/components/Const.ts
@@ -1,6 +1,8 @@
+import { isBrowser } from "./Util";
+
 export const clientId = "57f202d4c1f07138cd6a";
 export const clientSecret = "abef994f0f63495819116190ca6fdd05a4499226";
 export const oauthScope = "repo,read:packages";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = isBrowser ? `${location.protocol}//${location.hostname}` : "http://localhost:3000";
 
 export default { clientId, clientSecret, redirectUri };
