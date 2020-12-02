5b20f52f43e3cc5682db9bd44908cfa44eaebee7
takenoko <takenoko.tech@gmail.com>
2020-12-03 02:14:14 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable max-len */
+
 require("dotenv").config();
 
-console.log(`==================================`);
-console.log(`= Next config `);
-console.log(`==================================`);
+console.log("==================================");
+console.log("= Next config ");
+console.log("==================================");
+console.log(`process.env.CI = ${process.env.CI} [${typeof process.env.CI}]`);
 console.log(`process.env.GITHUB_PAGES = ${process.env.GITHUB_PAGES} [${typeof process.env.GITHUB_PAGES}]`);
-console.log(`\n`)
+console.log("\n");
+
+const isCi = process.env.CI === "true";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 module.exports = {
-    assetPrefix: process.env.GITHUB_PAGES == "true" ? "/apricot" : "",
+  assetPrefix: isGithubPages ? "/apricot" : "",
+  env: {
+    CLIENT_ID: isGithubPages ? process.env.CLIENT_ID_PROD : isCi ? process.env.CLIENT_ID_STG : "57f202d4c1f07138cd6a",
+    CLIENT_SECRET: isGithubPages ? process.env.CLIENT_SECRET_PROD : isCi ? process.env.CLIENT_SECRET_STG : "abef994f0f63495819116190ca6fdd05a4499226",
+  },
 };
--- src/components/Const.ts
+++ src/components/Const.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-globals */
 import { isBrowser } from "./Util";
 
-export const clientId = "57f202d4c1f07138cd6a";
-export const clientSecret = "abef994f0f63495819116190ca6fdd05a4499226";
+export const clientId = process.env.CLIENT_ID;
+export const clientSecret = process.env.CLIENT_SECRET;
 export const redirectUri = isBrowser ? `${location.protocol}//${location.host}` : "http://localhost:3000";
 export const oauthScope = "repo,read:packages,actions:read";
 
